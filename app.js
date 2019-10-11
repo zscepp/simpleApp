a3a53c3ce099ca7a3e3d8119377538f5a8db3965
zahid <zscepp@gmail.com>
2019-10-10 19:02:17 -0700

    initial commit

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+const port = process.env.port || 3000;
+
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res)=>{
+    res.render('pages/index')
+});
+
+app.get('/about', (req, res)=>{
+    res.render('pages/about');
+});
+
+app.listen(port, ()=>{
+    console.log('connmected to port ' + port);
+})
